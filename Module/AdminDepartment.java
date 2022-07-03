cf68cd175c28976b9fa523d5839f49a08b9e550d
thahem01 <107743849+thahem01@users.noreply.github.com>
2022-07-03 21:33:01 +0530

    Add files via upload

---
+++ Module/AdminDepartment.java
@@ -0,0 +1,22 @@
+package com.greatlearning.Module;
+
+public class AdminDepartment extends SuperDepartment {
+	public String departmentName() {
+		String s = "Admin Department";
+		return s;
+	}
+ 
+	public String getTodaysWork() {
+		String s1 = "Complete your documents Submission";
+		return s1;
+	}
+	
+	public String getWorkDeadline() {
+		String s2 = "Complete by EOD";
+		return s2;
+	}
+
+
+	}
+
+
